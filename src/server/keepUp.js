f90f99151e5d8085dfa75dbfc6090231fb473705
Henryk91 <heinrichk91@gmail.com>
2019-12-15 11:35:02 +0200

    Added KeepUp for heroku

---
+++ src/server/keepUp.js
@@ -0,0 +1,37 @@
+const https = require('https');
+
+let isRunning = false;
+function calcTime(offset) {
+  const d = new Date();
+  const utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+  const nd = new Date(utc + (3600000 * offset));
+  return nd.getHours();
+}
+
+function refresh(url, startHour, endHour, minuteInterval) {
+  const timeNow = calcTime('+2');
+  console.log('Time Now', timeNow, (timeNow > startHour), (timeNow < endHour));
+  if (timeNow > startHour && timeNow < endHour) {
+    console.log('Calling KeepUp!', new Date().toLocaleString());
+    https.get(url, (resp) => {
+      console.log('KeepUp Responce Status:', resp.statusCode);
+      console.log('');
+    });
+
+    setTimeout(() => {
+      refresh(url, startHour, endHour, minuteInterval);
+    }, (minuteInterval * 60 * 1000));
+  } else {
+    console.log('Done for the day!');
+  }
+}
+function KeepUp(url, startHour, endHour, minuteInterval) {
+  if (!isRunning) {
+    isRunning = true;
+    refresh(url, startHour, endHour, minuteInterval);
+  } else {
+    console.log('Already Running');
+  }
+}
+
+module.exports = KeepUp;
--- src/server/routes/getNotes.js
+++ src/server/routes/getNotes.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const Handler = require('../controllers/handlers.js');
+const KeepUp = require('../keepUp.js');
 
 const dbHandler = new Handler();
 
 router.get('/', (req, res) => {
+  KeepUp('https://henryk91-note.herokuapp.com/', 7, 19, 14);
+
   const { user } = req.query;
   if (user === 'all' || user === 'All') {
     dbHandler.getAllNotes(req, (docs) => {
@@ -25,5 +28,6 @@ router.get('/', (req, res) => {
   }
   // res.json(noteDummy)
 });
+
 
 module.exports = router;
