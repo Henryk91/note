168b30290aa6db92c66fc3d31b9b62a27213828e
Henryk91 <heinrichk91@gmail.com>
2020-03-28 14:11:19 +0200

    Restored upi/update

--- src/server/routes/updateNotes.js
+++ src/server/routes/updateNotes.js
@@ -7,10 +7,10 @@ module.exports = function (app) {
   app.post('/api/save', (req, res) => {
     dbHandler.newNote(req.body, dbResp => res.json({ Ok: dbResp }));
   });
-  // app.post('/api/update', (req, res) => {
-  //   dbHandler.updateNote(req, dbResp => res.json({ Ok: dbResp }));
-  //   res.json({ Ok: '100' });
-  // });
+  app.post('/api/update', (req, res) => {
+    dbHandler.updateNote(req, dbResp => res.json({ Ok: dbResp }));
+    res.json({ Ok: '100' });
+  });
   app.post('/api/update-one', (req, res) => {
     dbHandler.updateOneNote(req, (dbResp) => {
       res.json({ Ok: dbResp });
