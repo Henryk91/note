6338de8881173e8a3b3d406a04bf5243148f48d3
Henryk91 <heinrichk91@gmail.com>
2020-07-25 21:27:19 +0200

    email api expanded

--- src/server/routes/sendEmail.js
+++ src/server/routes/sendEmail.js
@@ -40,4 +40,39 @@ module.exports = function (app) {
         }
     });
   });
+
+  app.post('/api/emails',cors(), (req, res) => {
+    
+    console.log('Email send Request',);
+    let transport = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        auth: {
+            user: name,
+            pass: pass
+        }
+    });
+    console.log('req.body',req.body);
+    const info = req.body && req.body.text ? req.body.text : 'Message missing.'
+    const msgFrom = req.body && req.body.from ? req.body.from : 'mail@henryk.co.za'
+    const msgTo = req.body && req.body.to ? req.body.to : 'heinrichk91@gmail.com'
+    const subject = req.body && req.body.subject ? req.body.subject : 'From Website'
+    const message = {
+        from: msgFrom, // Sender address
+        to: msgTo,         // List of recipients
+        subject: subject, // Subject line
+        text: info // Plain text body
+    };
+
+    transport.sendMail(message, function(err, info) {
+
+        if (err) {
+          console.log('Email sending error:',err)
+          res.json({ Ok: '50' });
+        } else {
+            res.json({ Ok: '100' });
+          console.log('Success sending email!',info);
+        }
+    });
+  });
 };
