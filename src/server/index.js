86b4defe6b9af767528765c6cf4b590810b822c8
Henryk91 <heinrichk91@gmail.com>
2021-12-15 09:40:37 +0200

    test-sock

--- src/server/index.js
+++ src/server/index.js
@@ -7,11 +7,34 @@ const updateNotes = require('./routes/updateNotes');
 const userCheck = require('./routes/userCheck');
 const getDashData = require('./routes/getDashData');
 const sendEmail = require('./routes/sendEmail');
-const sock = require('./routes/sock');
+// const sock = require('./routes/sock');
 
 const app = express();
 
-sock(app)
+// sock(app)
+
+const INDEX = '/index.html';
+  // const PORT = process.env.PORT || 8090;
+const PORT = 8090;
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected');
+
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
