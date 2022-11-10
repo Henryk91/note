db32807f30b6168927532ceda1aad635ed408785
Henryk91 <heinrichk91@gmail.com>
2022-11-10 10:29:57 +0200

    trace warnings

--- src/server/controllers/handlers.js
+++ src/server/controllers/handlers.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB, { useNewUrlParser: true });
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology:true });
 
 const { Schema } = mongoose;
 
