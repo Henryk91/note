d1a1ddc1245b3c9d0949ce32ca57524d9c3e7f9c
Henryk91 <heinrichk91@gmail.com>
2024-02-27 09:29:19 +0100

    fix pasting

--- src/client/views/Components/EditNoteCheck/EditNoteCheck.js
+++ src/client/views/Components/EditNoteCheck/EditNoteCheck.js
@@ -309,6 +309,7 @@ function AutoCompleteTextArea(
     name
   }
   ) {
+  const [doReload, setDoReload] = useState(false)
   const [isBig, setIsBig] = useState()
   const [value, setValue] = useState()
 
@@ -320,9 +321,15 @@ function AutoCompleteTextArea(
     input = input.replaceAll("%0A", "\n")
 
     if(input.length > 17) {
-      if(!isBig) setIsBig(true)
+      if(!isBig) {
+        setIsBig(true)
+        setDoReload(!doReload)
+      }
     } else {
-      if(isBig) setIsBig(false)
+      if(isBig) {
+        setIsBig(false)
+        setDoReload(!doReload)
+      }
     }
 
     setValue(input)
@@ -334,13 +341,17 @@ function AutoCompleteTextArea(
     if(onChange) onChange(input)
   }
   const pasteListener = () => {
-    const element = document.getElementById(elementId)
-    element.addEventListener("paste", async (event) => {
+    const el = document.getElementById(elementId)
+    el.addEventListener("paste", async (event) => {
+      event.preventDefault()
       const text = await navigator.clipboard.readText();
+      const element = document.getElementById(elementId);
       setTimeout(() => {
-        const element = document.getElementById(elementId);
-        element.value = element.value + text;
-        element.focus()
+        // const value = element.value.length < 17? element.value + text: text;
+        const value = element.value + text;
+        setValue(value);
+        setIsBig(value.length > 17);
+        setDoReload(!doReload);
       },  10);
     })
   }
@@ -352,8 +363,7 @@ function AutoCompleteTextArea(
       element.focus()
     }
     pasteListener();
-    
-  },[isBig])
+  },[doReload])
 
   return isBig?(
     <textarea id={elementId} className={`${className} ${bigClassName}`} onChange={localChange} name={name} type="text" placeholder={placeholder} defaultValue={defaultValue}/>
