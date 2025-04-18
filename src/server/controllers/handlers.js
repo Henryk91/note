077c829b969447a95ad511626c329d53a5f68b4f
Henryk91 <heinrichk91@gmail.com>
2025-04-18 14:12:55 +0800

    allow multiple log paths

--- src/server/controllers/handlers.js
+++ src/server/controllers/handlers.js
@@ -319,9 +319,9 @@ module.exports = function () {
         console.log('userId',userId);
         if (doc.dataLable) {
             const referer = req.headers.referer;
-            const { pathname } = new URL(referer);
+            const pathname  = req.headers.path.replace("/api/log", "");
             const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-            const data = `Referer: ${referer}${pathname}\nIp: ${ip}\n SA Date: ${calcTimeNowOffset('+2')}\n https://ipapi.co/${ip}/`;
+            const data = `Referer: ${referer} ${pathname}\nIp: ${ip}\n SA Date: ${calcTimeNowOffset('+2')}\n https://ipapi.co/${ip}/`;
             let siteTag = 'Site one';
             if(referer){
               let siteName = referer.replace('http://','').replace('https://','')+"";
