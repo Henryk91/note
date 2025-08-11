44f53725644be34cc1292ad0a916ab3cf99b08d4
Henryk91 <heinrichk91@gmail.com>
2025-08-11 15:44:44 +0200

    test update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,55 +1,30 @@
 /* eslint-disable no-restricted-globals */
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable no-undef */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
-import { precacheAndRoute } from 'workbox-precaching';
-precacheAndRoute(self.__WB_MANIFEST);
 
 if (workbox) {
-  console.log('Workbox is loaded');
-
   workbox.skipWaiting();
   workbox.clientsClaim();
-  workbox.core.setCacheNameDetails({
-    prefix: 'noteList',
-    suffix: 'msiv1'
-  });
+  workbox.core.setCacheNameDetails({ prefix: 'noteList', suffix: 'msiv1' });
 
-  workbox.precaching.precacheAndRoute(self.__precacheManifest);
+  // If your build injects a precache manifest, keep this line:
+  workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 
   workbox.routing.registerRoute(
     /(\/$|\/\?.*$)/,
     workbox.strategies.networkFirst({
       cacheName: 'pages-cache',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxAgeSeconds: 1 * 24 * 60 * 60 // 1 Days
-        })
-      ]
+      plugins: [new workbox.expiration.Plugin({ maxAgeSeconds: 24 * 60 * 60 })],
     })
   );
-
-  // API Calls
-  // workbox.routing.registerRoute(/.*\/my_api\/v1.*/, workbox.strategies.staleWhileRevalidate({
-  //   cacheName: 'apis-cache',
-  //   plugins: [
-  //     new workbox.expiration.Plugin({
-  //       maxAgeSeconds: 1 * 24 * 60 * 60 // 1 Days
-  //     })
-  //   ]
-  // }));
 
   workbox.routing.registerRoute(
     /.*woff/,
     workbox.strategies.cacheFirst({
       cacheName: 'fonts-cache',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxAgeSeconds: 1 * 24 * 60 * 60 // 1 Days
-        })
-      ]
+      plugins: [new workbox.expiration.Plugin({ maxAgeSeconds: 24 * 60 * 60 })],
     })
   );
 } else {
-  console.log(' Workbox didn\'t load');
+  // eslint-disable-next-line no-console
+  console.log("Workbox didn't load");
 }
