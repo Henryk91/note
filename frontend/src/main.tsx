db28f83db696ea3dc1354b18a8b4efa23e9c1f95
Henryk91 <heinrichk91@gmail.com>
2025-12-09 11:56:03 +0200

    update react and dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
 import App from './client/App';
 import './client/app.css';
 import './serviceWorker';
 import { store } from './store';
 
-const root = document.getElementById('root');
-
-if (root) {
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  root,
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
-}
